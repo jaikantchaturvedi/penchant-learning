80585269926fde7d05eb6c82cc19c33a13ed7df9
Jaikant <jaikant@vidhema.com>
2026-01-06 09:32:41 +0530

    content up

--- Components/cards/Footer.tsx
+++ Components/cards/Footer.tsx
@@ -16,8 +16,33 @@ const FooterSection: React.FC = () => {
             </p>
 
             <div className="mt-4 space-y-1">
-              <p>info@listener.com</p>
-              <p><a href="tel:+919468643369">+9468643369</a></p>
+              <p className="flex items-center gap-2">
+                
+                <svg className="w-4 h-4 text-[#fcfcfc]" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l9 6 9-6M4 6h16a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2z"/>
+                </svg>
+
+                <a href="mailto:penchantlearnings@gmail.com">
+                  penchantlearnings@gmail.com
+                </a>
+              </p>
+
+              <p className="flex items-center gap-2 flex-wrap">
+                <svg className="w-4 h-4 text-[#fcfcfc]" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h2.28a2 2 0 011.94 1.51l.72 2.88a2 2 0 01-.45 1.86l-1.27 1.27a16 16 0 006.58 6.58l1.27-1.27a2 2 0 011.86-.45l2.88.72A2 2 0 0121 16.72V19a2 2 0 01-2 2h-1C9.72 21 3 14.28 3 6V5z"/>
+                </svg>
+
+                <a href="tel:+919468643369">+91 94686 43369</a>
+              </p>
+              <p className="flex items-center gap-2 flex-wrap">
+                <svg className="w-4 h-4 text-[#fcfcfc]" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h2.28a2 2 0 011.94 1.51l.72 2.88a2 2 0 01-.45 1.86l-1.27 1.27a16 16 0 006.58 6.58l1.27-1.27a2 2 0 011.86-.45l2.88.72A2 2 0 0121 16.72V19a2 2 0 01-2 2h-1C9.72 21 3 14.28 3 6V5z"/>
+                </svg>
+                <a href="tel:+916378907469">+91 63789 07469</a>
+              </p>
+
+              {/* <p><a href="mailto:penchantlearnings@gmail.com">penchantlearnings@gmail.com</a></p>
+              <p><a href="tel:+919468643369">+9468643369</a><a href="tel:+916378907469">+6378907469</a></p> */}
             </div>
           </div>
 
